80e7151161d5ec9f690e460865980c6d235b3d9e
fewang <fewang@local-admins-MacBook-Pro.local>
2015-04-29 19:38:06 +0800

    Code sync

---
+++ src/shanbay.py
@@ -0,0 +1,15 @@
+"""
+Translate word by shanbay.com
+"""
+
+
+__author__ = 'foomango@gmail.com'
+
+
+class ShanBay(object):
+    """Translate word by shanbay.com
+    """
+
+    def translate(word):
+        """
+        """
---
+++ test/test_shanbay.py
@@ -0,0 +1,14 @@
+"""Unit test for ShanBay"""
+
+__author__ = 'foomango@gmail.com'
+
+
+import sys
+sys.path.insert(0, '../src')
+
+from unittest import TestCase
+
+
+class ShanBayTestCase(TestCase):
+    """Unit test for ShanBay
+    """
